46ca0078c56d850921109163f6d4e2df9b73ae5b
Nacho131104 <nacholopezamat@gmail.com>
2025-06-29 13:23:06 +0200

    Update resolvers.ts

--- resolvers.ts
+++ resolvers.ts
@@ -1,5 +1,22 @@
+import { HotelModel } from "./types.ts";
+import { getTemp } from "./utils.ts";
 
 
 export const resolvers ={
-    
+
+    Hotel :{
+        id:(parent: HotelModel)  => parent._id!.toString() ,
+        intitial_temp:async(parent: HotelModel): Promise<number> =>{
+            const datos  = await getTemp(parent.latitude, parent.longitude);
+            return datos.min_temp
+        },
+        current_temp:async(parent:HotelModel):Promise<number> =>{
+            const datos  = await getTemp(parent.latitude, parent.longitude);
+            return datos.temp
+        },
+        luxury_score:(parent: HotelModel): number =>{
+            return parent.rooms * parent.stars
+        },
+    }
+
 }
